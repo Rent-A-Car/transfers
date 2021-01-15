2b082134ca28cc5b929d77f768ebdb3ae43e47c0
Rent-A-Car <56919673+Rent-A-Car@users.noreply.github.com>
2021-01-15 11:45:21 +0200

    website

--- data/js/main.js
+++ data/js/main.js
@@ -1,8 +1,22 @@
+/*
+sw
+*/
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./sw.js')
+      .then(() => navigator.serviceWorker.ready.then((worker) => {
+        worker.sync.register('syncdata');
+      }))
+      .catch((err) => console.log(err));
+}
+/*
+ sw end
+*/
+
 function openSidebarNav() {
 d=document;
-  d.getElementById("sidenav").style.width = "15rem";
-  d.getElementById("main-page").style.marginLeft = "15rem";
-  d.getElementById("navPages").style.marginLeft = "15rem";
+  d.getElementById("sidenav").style.width = "80%";
+  //d.getElementById("main-page").style.marginLeft = "15rem";
+  //d.getElementById("navPages").style.marginLeft = "15rem";
   d.getElementById("SidebarOpasity").style.width = "100%";
   d.getElementById("SidebarOpasity").style.opacity = "0.6";  
 
@@ -11,8 +25,8 @@ d=document;
 function closeSidebarNav() {
 d=document;
   d.getElementById("sidenav").style.width = "0";
-  d.getElementById("main-page").style.marginLeft= "0";
-  d.getElementById("navPages").style.marginLeft = "0";
+  //d.getElementById("main-page").style.marginLeft= "0";
+  //d.getElementById("navPages").style.marginLeft = "0";
   d.getElementById("SidebarOpasity").style.opacity = "0.5"; 
   d.getElementById("SidebarOpasity").style.width = "0px";
   
