65ac6f28adfde7b68e38c8ef6e0f8a6dbad0669f
Rent-A-Car <56919673+Rent-A-Car@users.noreply.github.com>
2021-01-15 17:21:40 +0200

    website

--- data/sw.js
+++ data/sw.js
@@ -1,5 +1,5 @@
 const CACHE_Version = 'v1'
-const CACHE= 'static-'+CACHE_Version
+const CACHE= 'sitecache-'+CACHE_Version
 const CacheAssets = [
 './index.html',
 './css/main.css',
@@ -19,6 +19,35 @@ self.addEventListener('activate', (event) => {
     console.log('Активирован');
 });
 
-self.addEventListener('fetch', (event) => {
-    console.log('Происходит запрос на сервер');
+
+self.addEventListener('fetch', function(evt) {
+ console.log('Происходит запрос на сервер');
+  // You can use `respondWith()` to answer immediately, without waiting for the
+  // network response to reach the service worker...
+  evt.respondWith(fromCache(evt.request));
+  // ...and `waitUntil()` to prevent the worker from being killed until the
+  // cache is updated.
+  evt.waitUntil(update(evt.request));
 });
+
+
+// Open the cache where the assets were stored and search for the requested
+// resource. Notice that in case of no matching, the promise still resolves
+// but it does with `undefined` as value.
+function fromCache(request) {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      return matching || Promise.reject('no-match');
+    });
+  });
+}
+
+// Update consists in opening the cache, performing a network request and
+// storing the new response data.
+function update(request) {
+  return caches.open(CACHE).then(function (cache) {
+    return fetch(request).then(function (response) {
+      return cache.put(request, response);
+    });
+  });
+}
