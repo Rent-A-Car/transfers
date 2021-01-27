77012bbcbff6133cc7febd56fdbf63374f0ee3ea
Rent-A-Car <56919673+Rent-A-Car@users.noreply.github.com>
2021-01-28 00:04:02 +0100

    website

--- data/js/installer.js
+++ data/js/installer.js
@@ -1,7 +1,9 @@
-//navigator.vendor
+
 // 1 chrome
 // 2 firefox
 // 3 safari
+// 4 ios not safari
+// 5 no suport
 const renderInstallModal = (type)=>{
 	modalstyles = document.createElement("style");
 	modalstyles.innerText=`
@@ -111,13 +113,19 @@ const renderInstallModal = (type)=>{
 	install_guide = ""
 	switch(type){
 		case 1:
-		install_guide='<p class="pwa-install-prompt__guide"><button class="btn btn-outline-success" id="beforeInstallButton">install</button></p></p>'
+		install_guide='<p class="pwa-install-prompt__guide"><button class="btn btn-outline-success" id="beforeInstallButton">install</button></p>'
 		break;
 		case 2:
-		install_guide=""
+		install_guide='<p class="pwa-install-prompt__guide"></p>'
 		break;
 		case 3:
 		install_guide = '<p class="pwa-install-prompt__guide">Just tap <svg class="pwa-install-prompt__guide__icon" viewBox="0 0 128 128" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><title>Share</title><path fill="#007AFF" d="M48.883,22.992L61.146,10.677L61.146,78.282C61.146,80.005 62.285,81.149 64,81.149C65.715,81.149 66.854,80.005 66.854,78.282L66.854,10.677L79.117,22.992C79.693,23.57 80.256,23.853 81.114,23.853C81.971,23.853 82.534,23.57 83.11,22.992C84.25,21.848 84.25,20.125 83.11,18.981L65.997,1.794C65.715,1.511 65.421,1.215 65.139,1.215C64.563,0.932 63.718,0.932 62.861,1.215C62.579,1.498 62.285,1.498 62.003,1.794L44.89,18.981C43.75,20.125 43.75,21.848 44.89,22.992C46.029,24.149 47.744,24.149 48.883,22.992ZM103.936,35.32L81.114,35.32L81.114,41.053L103.936,41.053L103.936,121.27L24.064,121.27L24.064,41.053L46.886,41.053L46.886,35.32L24.064,35.32C20.928,35.32 18.355,37.904 18.355,41.053L18.355,121.27C18.355,124.419 20.928,127.003 24.064,127.003L103.936,127.003C107.072,127.003 109.645,124.419 109.645,121.27L109.645,41.053C109.645,37.891 107.072,35.32 103.936,35.32Z" /></svg> then “Add to Home Screen”</p>'
+		break;
+		case 4:
+		install_guide='<p class="pwa-install-prompt__guide"><button class="btn btn-outline-success" onclick="">Open in Safari</button></p>'
+		break;
+		case 5:
+		install_guide='<p class="pwa-install-prompt__guide"><a href="https://caniuse.com/?search=A2HS" class="btn btn-outline-danger">Your browser not supported</a></p>'
 		break;
 	}
 template = `
@@ -138,12 +146,22 @@ template = `
     setTimeout(function(){ container.classList.add("is-active") }, 1000);
     
 }
-let ModalMode=0;
-
 const detectBrowserType=()=>{
 	
+let ua = navigator.userAgent.toLowerCase()
+isAndroid = ua.indexOf("android")>-1
+isIDevice  = /iphone|ipod|ipad/i.test(navigator.platform.toLowerCase());
+isFirefox = ua.indexOf("firefox")>-1 || ua.indexOf("fxios")>-1
 
-
+if (isAndroid && isFirefox){
+renderInstallModal(2)
+}else if(isIDevice && ua.indexOf("safari") > -1 && ua.indexOf("criOS") < 0 && !isFirefox){
+renderInstallModal(3)
+}else if(isIDevice){
+renderInstallModal(4)
+}else{
+renderInstallModal(5)
+}
 
 }
 
